a7b3912e8985ff45fae7f97ec60d490e3f29f6b0
KiraKiraAkira <161200063@smail.nju.edu.cn>
2020-05-30 18:10:17 +0800

    miss deque?

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
-public interface Deque {
+public interface Deque<Item> {
+    public void addFirst(Item x);
+    public void addLast(Item x);
+    default public boolean isEmpty(){
+        if(this.size()==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public int size();
+    public void printDeque();
+    public Item removeFirst();
+    public Item removeLast();
+    public Item get(int index);
 }
